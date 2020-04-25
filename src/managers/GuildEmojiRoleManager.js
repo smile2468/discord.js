c1f73e1318d7694102768b64a06fd78411569db5
Zero-Brazil734 <zero734kr@gmail.com>
2020-04-25 14:48:15 -0300

    (done) translation for src/managers/GuildEmojiRoleManager.js

--- src/managers/GuildEmojiRoleManager.js
+++ src/managers/GuildEmojiRoleManager.js
@@ -4,12 +4,12 @@ const { TypeError } = require('../errors');
 const Collection = require('../util/Collection');
 
 /**
- * Manages API methods for roles belonging to emojis and stores their cache.
+ * 역할들에 귀속된 이모지의 API 메소드를 관리하고 캐시에 저장합니다.
  */
 class GuildEmojiRoleManager {
   constructor(emoji) {
     /**
-     * The emoji belonging to this manager
+     * 이 매니저에 귀속된 이모지
      * @type {GuildEmoji}
      */
     this.emoji = emoji;
@@ -27,7 +27,7 @@ class GuildEmojiRoleManager {
   }
 
   /**
-   * The filtered collection of roles of the guild emoji
+   * 길드 이모지를 사용 가능한 역할 컬렉션
    * @type {Collection<Snowflake, Role>}
    * @private
    * @readonly
@@ -37,7 +37,7 @@ class GuildEmojiRoleManager {
   }
 
   /**
-   * The cache of roles belonging to this emoji
+   * 이 매니저에 귀속된 역할 캐시 
    * @type {Collection<Snowflake, Role>}
    * @readonly
    */
@@ -46,8 +46,8 @@ class GuildEmojiRoleManager {
   }
 
   /**
-   * Adds a role (or multiple roles) to the list of roles that can use this emoji.
-   * @param {RoleResolvable|RoleResolvable[]|Collection<Snowflake, Role>} roleOrRoles The role or roles to add
+   * 역할 또는 역할들을 이 이모지를 사용 가능한 역할 목록에 추가합니다.
+   * @param {RoleResolvable|RoleResolvable[]|Collection<Snowflake, Role>} roleOrRoles 추가할 역할 또는 역할들
    * @returns {Promise<GuildEmoji>}
    */
   add(roleOrRoles) {
@@ -64,8 +64,8 @@ class GuildEmojiRoleManager {
   }
 
   /**
-   * Removes a role (or multiple roles) from the list of roles that can use this emoji.
-   * @param {RoleResolvable|RoleResolvable[]|Collection<Snowflake, Role>} roleOrRoles The role or roles to remove
+   * 역할 또는 역할들을 이 이모지를 사용 가능한 역할 목록에서 제거합니다.
+   * @param {RoleResolvable|RoleResolvable[]|Collection<Snowflake, Role>} roleOrRoles 제거할 역할 또는 역할들
    * @returns {Promise<GuildEmoji>}
    */
   remove(roleOrRoles) {
@@ -82,16 +82,16 @@ class GuildEmojiRoleManager {
   }
 
   /**
-   * Sets the role(s) that can use this emoji.
-   * @param {Collection<Snowflake, Role>|RoleResolvable[]} roles The roles or role IDs to apply
+   * 이 이모지를 사용 가능한 역할 목록을 덮어씌웁니다.
+   * @param {Collection<Snowflake, Role>|RoleResolvable[]} roles 덮어씌울 역할들 또는 역할들 ID
    * @returns {Promise<GuildEmoji>}
    * @example
-   * // Set the emoji's roles to a single role
+   * // 한 역할만 이 이모지를 사용 가능하도록 덮어씌웁니다
    * guildEmoji.roles.set(['391156570408615936'])
    *   .then(console.log)
    *   .catch(console.error);
    * @example
-   * // Remove all roles from an emoji
+   * // 이모지를 사용 가능한 역할 목록에서 모든 역할들을 제거합니다
    * guildEmoji.roles.set([])
    *    .then(console.log)
    *    .catch(console.error);
@@ -107,8 +107,8 @@ class GuildEmojiRoleManager {
   }
 
   /**
-   * Patches the roles for this manager's cache
-   * @param {Snowflake[]} roles The new roles
+   * 타입 검사를 무시하고 역할 목록을 캐시에 귀속합니다
+   * @param {Snowflake[]} roles 새로운 역할들
    * @private
    */
   _patch(roles) {
